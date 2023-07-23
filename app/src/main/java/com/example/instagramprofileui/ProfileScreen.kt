6706ee56806946d4c22b675dc651b9d4eadab8da
gulnara_azizova <gazizova12047@ada.edu.az>
2023-07-23 21:50:11 +0400

    Completed Top Bar section of UI app..

--- app/src/main/java/com/example/instagramprofileui/ProfileScreen.kt
+++ app/src/main/java/com/example/instagramprofileui/ProfileScreen.kt
@@ -1,6 +1,5 @@
 package com.example.instagramprofileui
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
@@ -20,7 +19,7 @@ fun ProfileScreen(){
         .fillMaxSize()
         .padding(20.dp)
     ){
-        TopBar(name = "gulnara_azizova")
+        TopBar(name = "gulnaragallery")
         Spacer(modifier = Modifier.height(4.dp))
     }
 }
@@ -36,36 +35,48 @@ fun TopBar(
         modifier = modifier
             .fillMaxWidth()
     ) {
+        Icon(
+            modifier = modifier.size(25.dp),
+            painter = painterResource(id = R.drawable.baseline_arrow_back),
+            contentDescription = "Return back",
+            tint = Color.Black
+        )
         Text(
             text = name,
             overflow = TextOverflow.Ellipsis,
             fontWeight = FontWeight.Bold,
             fontSize = 20.sp
         )
-        Row(
-            horizontalArrangement = Arrangement.End,
-            modifier = Modifier
-                .background(Color.Black)
-                .fillMaxWidth()
-        ) {
-            Icon(
-                painter = painterResource(id = R.drawable.baseline_notifications),
-                contentDescription = "Notifications",
-                tint = Color.White,
-                modifier = modifier.size(30.dp)
-            )
-            Icon(
-                painter = painterResource(id = R.drawable.baseline_create),
-                contentDescription = "Create",
-                tint = Color.White,
-                modifier = modifier.size(30.dp)
-            )
-            Icon(
-                painter = painterResource(id = R.drawable.baseline_menu),
-                contentDescription = "Menu",
-                tint = Color.White,
-                modifier = modifier.size(30.dp)
-            )
-        }
+        Icon(
+            painter = painterResource(id = R.drawable.baseline_notifications),
+            contentDescription = "Notifications",
+            tint = Color.Black,
+            modifier = modifier.size(30.dp)
+        )
+        Icon(
+            painter = painterResource(id = R.drawable.baseline_create),
+            contentDescription = "Create",
+            tint = Color.Black,
+            modifier = modifier.size(30.dp)
+        )
+        Icon(
+            painter = painterResource(id = R.drawable.baseline_menu),
+            contentDescription = "Menu",
+            tint = Color.Black,
+            modifier = modifier.size(30.dp)
+        )
+    }
+}
+
+@Composable
+fun ProfileSection(
+    modifier: Modifier = Modifier
+){
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+
+    ) {
+
     }
 }
